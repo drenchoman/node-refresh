5bf793309e01acd537198c9afde2894aece363ec
Oscar Harron <oscarharron@hotmail.com>
2025-01-18 11:42:54 +1300

    add custom error handling

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.use('/authors', authorRouter);
 app.use('/books', bookRouter);
 app.use('/', indexRouter);
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
