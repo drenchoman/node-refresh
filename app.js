b19b857704dcf9495532c99e180c1f5f39918a9f
Oscar Harron <oscarharron@hotmail.com>
2025-01-18 12:07:57 +1300

    test middleware funcitonality

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ const indexRouter = require('./routes/indexRouter');
 const authorRouter = require('./routes/authorRouter');
 const bookRouter = require('./routes/bookRouter');
 
+// Middleware runs before reaching the routes
+app.use(m1);
+app.use(m2);
+app.use(m3);
+
 app.use('/authors', authorRouter);
 app.use('/books', bookRouter);
 app.use('/', indexRouter);
+
+function m1(req, res, next) {
+  console.log('Middleware 1');
+  next();
+}
+
+function m2(req, res, next) {
+  console.log('Middleware 2');
+  next();
+}
+
+function m3(req, res, next) {
+  console.log('Middleware 3');
+  next();
+}
 
 app.use((err, req, res, next) => {
   console.error(err);
