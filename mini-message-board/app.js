b29989c43965aa0c61b2fb77ba0a3029f75f70f3
Oscar Harron <oscarharron@hotmail.com>
2025-01-19 15:29:32 +1300

    add boilerplate for mini-message-board

---
+++ mini-message-board/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const indexRouter = require('./routes/indexRouter');
+const path = require('node:path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
