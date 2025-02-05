e14c6b2abf8e539608390512179405588fcb6a44
Oscar Harron <oscarharron@hotmail.com>
2025-02-05 17:38:00 +1300

    start validation for emoji form

--- inventory-application/controller/emojiController.js
+++ inventory-application/controller/emojiController.js
@@ -1,4 +1,31 @@
 const db = require('../db/queries');
+const expressAsyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
+const alphaErr = 'must only contains letters';
+const emptyErr = 'must not be empty';
+const nameLengthErr = 'must be between 1 and 15 characters';
+const descLengthErr = 'must be beween 1 and 300 characters';
+
+const validateEmoji = [
+  body('name')
+    .trim()
+    .isAlpha()
+    .withMessage(`Emoji name ${alphaErr}`)
+    .isLength({ min: 1, max: 15 })
+    .withMessage(`Emoji name ${nameLengthErr}`)
+    .notEmpty()
+    .withMessage(`Emoji name ${emptyErr}`),
+  body('description')
+    .trim()
+    .escape()
+    .isLength({ min: 1, max: 300 })
+    .withMessage(`Description ${descLengthErr}`),
+  body('info_url')
+    .trim()
+    .isURL({ protocols: ['https', 'http'] })
+    .withMessage('Url must be http or https'),
+];
 
 async function getEmojis(req, res) {
   const emojis = await db.getAllEmojis();
