fa279abbd9f6428cd515acd458800e6437fab90c
Oscar Harron <oscarharron@hotmail.com>
2025-02-27 17:49:48 +1300

    add boilerplate for server

---
+++ auth-basics/app.js
@@ -0,0 +1,29 @@
+const path = require('node:path');
+const { Pool } = require('pg');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const { DB_HOST, DB_USER, DB_DATABASE, DB_PASSWORD, DB_PORT, PORT } =
+  process.env;
+const pool = new Pool({
+  host: DB_HOST,
+  user: DB_USER,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(
+  session({ secret: 'cats', resave: false, saveUninitialized: false })
+);
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
