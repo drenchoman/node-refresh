0997a0b26f17402e6f35ab11c7f0e1b59da6ec54
Oscar Harron <oscarharron@hotmail.com>
2025-03-04 17:50:12 +1300

    add passport local strategy

--- auth-basics/app.js
+++ auth-basics/app.js
@@ -8,6 +8,46 @@ const { PORT } = process.env;
 
 const signUpRouter = require('./routes/sign-up');
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        'SELECT * FROM users WHERE username = $1',
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      if (user.password != password) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+// Saves session / cookie using the id of the user
+
+passport.serlializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query(
+      'SELECT * FROM users WHERE id = $1',
+      [id]
+    );
+    const user = rows[0];
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
